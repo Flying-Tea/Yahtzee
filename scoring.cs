3e1070ce8f50dbdd1cc39d1b38b850cc3f7d55ba
sidneyproceee <sidn5260@scdsb.on.ca>
2025-11-06 14:05:16 -0500

    I started my cs file
    you smell

---
+++ scoring.cs
@@ -0,0 +1,22 @@
+class Scoring(){
+
+    private bool one = false;
+    private bool two = false;
+    private bool three = false;
+    private bool four = false;
+    private bool five = false;
+    private bool six = false;
+
+    private bool tKind = false;
+    private bool fKind = false;
+    private bool fiveKind = false;
+    private bool fHouse = false;
+    private bool smStraight = false;
+    private bool lgStraight = false;
+    private bool yahtzee = false;
+    private bool chance = false;
+
+    public Scoring(){
+        
+    }
+}
